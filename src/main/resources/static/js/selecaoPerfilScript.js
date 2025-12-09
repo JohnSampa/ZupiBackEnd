35a7e098e162e42192edc276f7583835edc95607
Jonathan Sampaio <aurijona192@gmail.com>
2025-12-09 14:34:01 -0300

    corrigindo rota perfil em seleção de perfil

--- src/main/resources/static/js/selecaoPerfilScript.js
+++ src/main/resources/static/js/selecaoPerfilScript.js
@@ -60,7 +60,7 @@
               <p class="card-text text-muted small"><strong>Condição:</strong> ${child.condition || 'Não informado'}</p>
               <div class="mt-auto">
                 <a href="menu-jogos.html" class="btn btn-primary btn-sm" role="button" >Acessar Jogos</a>
-                <a href="/perfil-criancas" class="btn btn-primary btn-sm" role="button" onclick="createChildLocalID(${child.id})">Perfil</a>
+                <a href="/perfil" class="btn btn-primary btn-sm" role="button" onclick="createChildLocalID(${child.id})">Perfil</a>
                 <a href="/relatorios" class="btn btn-outline-primary btn-sm" role="button" onclick="createChildLocalID(${child.id})">Ver Relatório</a>
               </div>
             </div>
