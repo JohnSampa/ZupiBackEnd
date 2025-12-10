4d69e1ce49f4bb147f8fedaff8b03bd87eba9fef
Jonathan Sampaio <aurijona192@gmail.com>
2025-12-10 03:48:31 -0300

    Resetando repositório com novos arquivos

--- src/main/java/senai/tcc/zupiapi/zupibackend/repositories/ChildReportScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package senai.tcc.zupiapi.zupibackend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import senai.tcc.zupiapi.zupibackend.model.ChildReportScore;
-
-public interface ChildReportScoreRepository extends JpaRepository<ChildReportScore, Integer> {
-}
--- src/main/java/senai/tcc/zupiapi/zupibackend/repositories/SkillAreaRepository.java
+++
@@ -1,8 +0,0 @@
-package senai.tcc.zupiapi.zupibackend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import senai.tcc.zupiapi.zupibackend.model.SkillArea;
-
-public interface SkillAreaRepository extends JpaRepository<SkillArea, Integer> {
-    SkillArea findByName(String name);
-}
